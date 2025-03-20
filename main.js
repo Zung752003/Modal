79ecb3ac8a44a6ebf4fb0e8269c0f4dfdc7947ad
Nguyen Hoang <chungtawf@gmail.com>
2025-03-20 16:59:39 +0700

    update open and close

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const modal = $("#modal");
+let currentModal = null;
 
-$("#openModal").onclick = function(){
-    modal.classList.add("show")
-}
+$$('[data-modal]').forEach(btn => {
+    btn.onclick = function(){
+        const modal = $(this.dataset.modal);
+        if(modal){
+            modal.classList.add("show")
+        } else{
+            console.error(`${this.dataset.modal} does not exist!`)
+        }
 
-$("#closeModal").onclick = function(){
-     modal.classList.remove("show")
-}
-
-modal.onclick = function(e){
-    if(e.target === modal){
-        modal.classList.remove("show")
+        currentModal = modal;
     }
-}
+})
+
+$$('.modal-close').forEach(btn => {
+    btn.onclick = function(){
+        const modal = btn.closest('.modal-backdrop');
+        if(modal){
+            modal.classList.remove("show")
+        }
+        currentModal = null;
+    }
+})
+
+$$('.modal-backdrop').forEach(modal => {
+    modal.onclick = function(e){
+        if(e.target === this){
+            this.classList.remove("show")
+        }
+        currentModal = null;
+    }
+})
 
 document.addEventListener("keydown", function(e){
-    if(e.key === "Escape"){
-        modal.classList.remove("show")
+    if(e.key === "Escape" && currentModal){
+        currentModal.classList.remove("show");
+        currentModal = null;
     }
 })
 
